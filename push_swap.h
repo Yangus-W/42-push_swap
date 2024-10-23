b823fc26194a11fbb545bf5dbf466c90db0e5aa2
Yang Wang <wangyang.felix@hotmail.com>
2024-10-23 16:34:43 +0200

    add struct

--- push_swap.h
+++ push_swap.h
@@ -6,7 +6,7 @@
 /*   By: yawang <yawang@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/23 16:11:45 by yawang            #+#    #+#             */
-/*   Updated: 2024/10/23 16:14:29 by yawang           ###   ########.fr       */
+/*   Updated: 2024/10/23 16:31:33 by yawang           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,5 +15,22 @@
 # include "ft_printf/ft_printf.h"
 # include "ft_printf/libft/libft.h"
 # include <stdlib.h>
+
+typedef struct s_init
+{
+	char	**arguments;
+	int		*stack_a;
+	int		*stack_b;
+	int		*tmp;
+	int		int_a;
+	int		int_b;
+	int		min;
+	int		i;
+	int		j;
+	int		counter;
+	int		small_i;
+	int		big_i;
+	float	median;
+}	t_init;
 
 #endif
