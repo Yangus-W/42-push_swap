4967e10444696281df108029427b652daa5ac93b
Yang Wang <wangyang.felix@hotmail.com>
2024-10-23 16:14:52 +0200

    create header file

---
+++ push_swap.h
@@ -0,0 +1,19 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   push_swap.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: yawang <yawang@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/23 16:11:45 by yawang            #+#    #+#             */
+/*   Updated: 2024/10/23 16:14:29 by yawang           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+# include "ft_printf/ft_printf.h"
+# include "ft_printf/libft/libft.h"
+# include <stdlib.h>
+
+#endif
